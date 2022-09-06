b5e29cc3b916cc1361b8ecd140cf347440d641e6
hiramramirez <hiram.j.ramirez@gmail.com>
2022-09-06 10:37:31 -0500

    advancing

---
+++ auth.js
@@ -0,0 +1,12 @@
+const auth = firebase.auth()
+const provider = new firebase.auth.GoogleAuthProvider()
+
+auth.languageCode = "es"
+
+export async function login() {
+  try {
+
+  } catch (error) {
+
+  }
+}
